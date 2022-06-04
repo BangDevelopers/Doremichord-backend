a3ebc60adb31c3ef5375892fddbafd0cfe5c3f05
yinoqifu00 <91078730+yinoqifu00@users.noreply.github.com>
2022-06-04 22:45:13 +0800

    Update LrcLoader.cpp
    fix some QT related problems

--- src/sources/LrcLoader.cpp
+++ src/sources/LrcLoader.cpp
@@ -6,17 +6,28 @@
 #include "LrcModel.h"
 #include "LrcLoader.h"
 
-LrcModel LrcLoader::load(const char *path) {
-    LrcModel model;
-    std::ifstream file(path);
-    std::string line;
-    while (std::getline(file, line)) {
+
+LrcModel* LrcLoader::load(const char *path) {
+    LrcModel *Model=new LrcModel();
+
+    QFile File(path);
+    
+
+    
+
+   QString Line;
+    while (!File.atEnd()) {
+        Line=File.readLine();
+        Line=Line.mid(0,Line.length()-1);
+        std::string line=Line.toStdString();
         if (line.empty()) {
             continue;
         }
+
         std::regex regex(R"(\[(\d{2}):(\d{2})(\.(\d{2}))?](.*))");
         std::smatch match;
         if (std::regex_match(line, match, regex)) {
+
             int minute = std::stoi(match[1]);
             int second = std::stoi(match[2]);
             int millisecond = 0;
@@ -24,11 +35,14 @@ LrcModel LrcLoader::load(const char *path) {
                 millisecond = std::stoi(match[4]);
             }
             std::string content = match[5];
+           
+
             int time = minute * 60 * 1000 + second * 1000 + millisecond * 10;
-            model.add(time, content);
+            (*Model).add(time, content);
         } else{
-            model.add(-1, line);
+            Model->add(-1, line);
         }
     }
-    return model;
+
+    return Model;
 }
